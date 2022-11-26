f255c499950782671fc7d18e62d4921cfbade752
Johnny <JohnnyLieu@yahoo.com>
2022-11-26 13:35:56 -0800

    displays user and dealer's hand

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import random
 import art
-print(art.logo)
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+dealers_hand = []
+players_hand = []
+
+print(art.logo)
+
+dealers_hand.append(random.choice(cards))
+players_hand.append(random.choice(cards))
+
+print(f'Your cards: {players_hand}, current score: {sum(players_hand)}')
+print(f'Dealder\'s cards: {dealers_hand}, current score: {sum(dealers_hand)}')
